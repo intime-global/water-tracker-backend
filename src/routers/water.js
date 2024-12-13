d0aebe5a7bc05ac4f26798332d9f4d1857a4026d
Hanna Lakus <annalakus@ukr.net>
2024-12-13 02:09:32 +0000

    updated add waterNote service

--- src/routers/water.js
+++ src/routers/water.js
@@ -14,8 +14,11 @@ import {
 } from '../controllers/water.js';
 
 import { isValidId } from '../middlewares/isValidId.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = Router();
+
+router.use(authenticate);
 
 router.post(
   '/',
