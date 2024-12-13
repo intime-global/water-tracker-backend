cca9eadf16b7afafb8b2500734cb8abcf8befad0
Olexandr-L45D <litvinenko1978aleks@gmail.com>
2024-12-13 09:15:03 +0200

    add params to valid chema

--- src/validation/user.js
+++ src/validation/user.js
@@ -5,23 +5,28 @@ import { genderEnum } from '../constants/users.js';
 import { emailRegexp } from '../constants/users.js';
 
 export const updateUsersCard = Joi.object({
-    name: Joi.string().min(3).max(20).messages({
+    name: Joi.string().min(3).max(32).messages({
         'string.base': 'Username should be a string',
         'string.min': 'Username should have at least {#limit} characters',
         'string.max': 'Username should have at most {#limit} characters',
 
     }),
-    email: Joi.string().pattern(emailRegexp).min(10).max(30).required().messages({
+    email: Joi.string().pattern(emailRegexp).min(8).max(64).required().messages({
         'string.min': 'email should have at least {#limit} characters',
         'string.max': 'email should have at most {#limit} characters',
         'any.required': 'email is required',
     }),
-    password: Joi.string().min(10).max(20).messages({
+    password: Joi.string().min(8).max(64).messages({
         'string.min': 'password should have at least {#limit} characters',
         'string.max': 'password should have at most {#limit} characters',
         'any.required': 'password is required',
     }),
-    gender: Joi.string().valid(...genderEnum),
+    oldPassword:Joi.string().min(8).max(64).messages({
+        'string.min': 'oldPassword should have at least {#limit} characters',
+        'string.max': 'oldPassword should have at most {#limit} characters',
+        'any.required': 'oldPassword is required',
+    }),
+    gender: Joi.string().valid(...genderEnum), enum: ['male', 'female'],
     daylyNorm: Joi.string().min(1000).max(15000),
     photo: Joi.string(),
 });
