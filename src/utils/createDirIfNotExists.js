a9f354d45946a1f10f35b8d8a6f25c217e4ae129
Denis Chernyak <chernyak.ds@gmail.com>
2024-12-24 14:21:06 +0200

    fixes 1

--- src/utils/createDirIfNotExists.js
+++ src/utils/createDirIfNotExists.js
@@ -4,7 +4,6 @@ export const createDirIfNotExists = async (url) => {
   try {
     await fs.access(url);
   } catch (error) {
-    console.log(error.code, 'err code, create the dir');
     if (error.code === 'ENOENT') {
       await fs.mkdir(url);
     }
